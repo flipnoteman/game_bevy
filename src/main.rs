3f4f09ee4756349360a68ce6865200176dd23247
Sam Smith <sammys.email.home@gmail.com>
2023-03-01 01:11:52 -0600

    Added LDtk support
    Added Level Design Toolkit support, and added a test level one.
    
    Needs to be added to the game to draw (i have no idea how to do this)

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use crate::util::asset_handling::*;
 use crate::game_objects::entities::player::*;
 use crate::game_objects::entities::player_movement::player_move;
 
+use bevy_ecs_ldtk::prelude::*;
+
 mod util;
 
 fn main() {
@@ -21,18 +23,47 @@ fn main() {
         )
         .add_state(AssetStates::AssetLoading) // To define the initial state
         .insert_resource(Msaa { samples: 1 }) // Adds anti aliasing
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugin(LdtkPlugin)
         // Here we add system sets so we can make sure we only call the sprites if the assets have been completely loaded
         // "AssetStates::Next" is the state AFTER the sprite sheets are done loading, so systems that are called "on_enter"
         // will only call once, right after the assets are successfully loaded, and systems with "on_update" are called once every
         // game update cycle, just like add_startup_system and add_system but with proper error checking
         .add_system_set(SystemSet::on_enter(AssetStates::Next).with_system(spawn_camera))
         .add_system_set(SystemSet::on_enter(AssetStates::Next).with_system(Player::spawn_player))
+        .add_system_set(SystemSet::on_enter(AssetStates::Next).with_system(ldtk_setup))
+        .insert_resource(LevelSelection::Index(0))
+        .register_ldtk_entity::<LevelBundle>("LevelIdentifier")
         .add_system_set(SystemSet::on_update(AssetStates::Next).with_system(player_move))
         .add_system_set(SystemSet::on_update(AssetStates::Next).with_system(animate_sprite_system))
         .add_system_set(SystemSet::on_update(AssetStates::Next).with_system(Player::change_animation))
         .add_system_set(SystemSet::on_update(AssetStates::Next).with_system(move_camera))
         .run();
+}
+
+fn ldtk_setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    commands.spawn(LdtkWorldBundle {
+       ldtk_handle: asset_server.load("Level/level_one.ldtk"),
+        ..default()
+    });
+}
+
+#[derive(Default, Component)]
+struct ComponentA;
+
+#[derive(Default, Component)]
+struct ComponentB;
+
+#[derive(Bundle, LdtkEntity)]
+pub struct LevelBundle {
+    a: ComponentA,
+    b: ComponentB,
+    #[sprite_sheet_bundle]
+    #[bundle]
+    sprite_bundle: SpriteSheetBundle
 }
 
 
